6b7a557c97d0e55cfabe988b417819f28a47a491
aa-naoki <zhugangzhiji@gmail.com>
2021-06-10 21:51:40 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
+  validates :password,        format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
   validates :nickname,        presence: true
   validates :first_name,      presence: true
   validates :last_name,       presence: true
-  validates :first_pseudonym, presence: true
-  validates :last_pseudonym,  presence: true
+  validates :first_pseudonym,  format: {with: /\A[ァ-ン]+\z/}, presence: true
+  validates :last_pseudonym,  presence: true, format: {with: /\A[ァ-ン]+\z/}
   validates :birth_date,      presence: true
 end
