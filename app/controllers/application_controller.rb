6a4149d0481e878654ba7b75a083bd20a88d5f7a
aa-naoki <zhugangzhiji@gmail.com>
2021-06-10 16:58:00 +0900

    新規登録機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def basic_auth
@@ -10,6 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:niclname, :first_name, :last_name, :first_pseudonym, :last_pseudonym, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_pseudonym, :last_pseudonym, :birth_date])
   end
 end
