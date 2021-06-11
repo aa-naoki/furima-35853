f8964905276010feb78a931f4e66ea2af97cfb54
aa-naoki <zhugangzhiji@gmail.com>
2021-06-11 19:17:46 +0900

    表示させるエラーメッセージの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  validates :password,        format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
+  validates :password,        format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'is invalid. Include both letters and numbers'} 
   validates :nickname,        presence: true
   validates :first_name,      presence: true
   validates :last_name,       presence: true
-  validates :first_pseudonym,  format: {with: /\A[ァ-ン]+\z/}, presence: true
-  validates :last_pseudonym,  presence: true, format: {with: /\A[ァ-ン]+\z/}
+  validates :first_pseudonym, presence: true, format: {with: /\A[ァ-ン]+\z/, message: 'is invalid. Input full-width katakana characters'}
+  validates :last_pseudonym,  presence: true, format: {with: /\A[ァ-ン]+\z/, message: 'is invalid. Input full-width katakana characters'}
   validates :birth_date,      presence: true
 end
