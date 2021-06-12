e24251e5ef2080560c11ac9487e7ef51d7e69461
aa-naoki <zhugangzhiji@gmail.com>
2021-06-12 15:13:01 +0900

    item バリデーションの設定

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  with_option presence: true do
+    validates :item_name
+    validates :explanation
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+    validates :price
+  end
+end
