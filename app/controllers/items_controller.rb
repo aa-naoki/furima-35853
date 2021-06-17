902f0d8c50117b53b3ad08db26bc486cb69e5682
aa-naoki <zhugangzhiji@gmail.com>
2021-06-17 18:08:13 +0900

    destroyアクションのコード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_params, only: [:show, :edit, :update, :destroy]
-  before_action :truth_user, only: [:edit, :update]
+  before_action :truth_user, only: [:edit, :update, :destroy]
   def index
     @items = Item.order('created_at DESC')
   end
