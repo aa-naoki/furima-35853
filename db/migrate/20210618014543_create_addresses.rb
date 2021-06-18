6d001b73841f5df081323e8434adc74eac838c60
aa-naoki <zhugangzhiji@gmail.com>
2021-06-18 15:06:29 +0900

    orderコントローラー・ルーティングの設定

--- db/migrate/20210618014543_create_addresses.rb
+++ db/migrate/20210618014543_create_addresses.rb
@@ -1,12 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.strind     :postal_code,      null: false
+      t.string     :postal_code,      null: false
       t.integer    :shipping_area_id, null: false
-      t.strind     :municipality,     null: false
-      t.strind     :house_number,     null: false
-      t.strind     :building
-      t.strind     :phone_number,     null: false
+      t.string     :municipality,     null: false
+      t.string     :house_number,     null: false
+      t.string     :building
+      t.string     :phone_number,     null: false
       t.references :purchase,         null: false, foreign_key: true
       t.timestamps
     end
