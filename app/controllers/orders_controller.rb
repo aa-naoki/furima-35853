af1fe16005271f3dbe62ad887d5e1e0f063c891b
aa-naoki <zhugangzhiji@gmail.com>
2021-06-22 17:25:19 +0900

    PurchaseAddressの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :set_params, only: [:index, :create]
+
   def index
-    @order = Item.find_by(id: params[:item_id] )
+    @purchase_address = PurchaseAddress.new()
+  end
+  
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+  private
+  
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :shipping_area_id, :municipality,
+                                             :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+  
+  def set_params
+    @order = Item.find(params[:item_id])
   end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,9 +2,4 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one    :address
-
-  with_options presence: true do
-    validates :user
-    validates :item
-  end
 end
